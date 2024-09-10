6bc34fd5b0ad030293b33e68833ab116eaf8bf85
rtfmkiesel <79413747+rtfmkiesel@users.noreply.github.com>
2024-09-10 21:26:53 +0200

    Enable the button once the loading of drivers is complete, new build

--- main.go
+++ main.go
@@ -45,14 +45,23 @@ func MatchHashWrapper(this js.Value, p []js.Value) interface{} {
 }
 
 func main() {
+	// Get the button from the DOM
+	jsDoc := js.Global().Get("document")
+	folderButton := jsDoc.Call("getElementById", "select-folder-button")
+
 	fmt.Println("Loading drivers")
 	if err := loldrivers.LoadDrivers(); err != nil {
 		fmt.Println(err)
+		folderButton.Set("innerHTML", "There was an error fetching the driver data, check the console")
 		return
 	}
 
-	fmt.Println("Registering lookupHash")
+	fmt.Println("Registering lookupHash function")
 	js.Global().Set("lookupHash", js.FuncOf(MatchHashWrapper))
+
+	// Enable the button if we get here
+	folderButton.Set("innerHTML", "Select folder")
+	folderButton.Set("disabled", false)
 
 	// Do not quit
 	select {}
